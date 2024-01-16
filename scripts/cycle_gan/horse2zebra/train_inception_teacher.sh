747680f5fcee40b48b298d3f19fd00297b04ab10
chenl <chanlooks@outlook.com>
2024-01-16 07:26:55 +0000

    Update batch size, number of epochs, and GPU IDs in train_inception_teacher.sh

--- scripts/cycle_gan/horse2zebra/train_inception_teacher.sh
+++ scripts/cycle_gan/horse2zebra/train_inception_teacher.sh
@@ -5,10 +5,10 @@ python train.py --dataroot database/horse2zebra \
   --netG inception_9blocks \
   --real_stat_A_path real_stat/horse2zebra_A.npz \
   --real_stat_B_path real_stat/horse2zebra_B.npz \
-  --batch_size 32 \
-  --nepochs 500 --nepochs_decay 500 \
+  --batch_size 1 \
+  --nepochs 150  --nepochs_decay 150 \
   --num_threads 32 \
-  --gpu_ids 0,1,2,3,4,5,6,7 \
+  --gpu_ids 1 \
   --norm_affine \
   --norm_affine_D \
   --channels_reduction_factor 6 \
